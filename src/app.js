ddc7b06fd272239b74fbc09bca9d73e74503c3c4
Hammad Balkhi <hammadbalkhi@Hammads-MacBook-Pro.local>
2018-10-14 15:41:59 -0400

    Removed all dummy data

--- src/app.js
+++ src/app.js
@@ -2,40 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import RouterApp from './routes/RouterApp';
 import reduxStore from './store/store.js';
-import { Provider, connect } from 'react-redux';
-import { addExpense } from './actions/expenses.js';
-import moment from 'moment';
+import { Provider } from 'react-redux';
 import 'react-dates/lib/css/_datepicker.css';
 import './styles/style.scss';
 
-
 const store = reduxStore();
 
-store.dispatch(addExpense({
-    description: 'internet bill',
-    amount: 350,
-    createdAt: moment().format()
-    })
-);
-
-store.dispatch(addExpense({
-    description: 'rent',
-    amount: 100,
-    createdAt: moment().format()
-    })
-);
-
-// store.dispatch(changeText('int'));
-
-
-// const test = getVisibleExpenses(store.getState().expenses, store.getState().filters);
-
-// console.log(test);
-
-const jsx = (
+const APP = (
     <Provider store={store}>
         <RouterApp />
     </Provider>
 );
-ReactDOM.render(jsx, document.getElementById('root'));
+ReactDOM.render(APP, document.getElementById('root'));
 

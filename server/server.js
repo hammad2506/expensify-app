7f97794ba433ffc0e8c4ea197fa7365b58e48f22
Hammad Balkhi <hammadbalkhi@Hammads-MacBook-Pro.local>
2018-10-14 19:15:29 -0400

    heroku config added

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const path = require('path');
 const app = express();
 const PublicPath = path.join(__dirname, '..', 'public');
+const port = process.env.PORT || 3000;
 
 app.use(express.static(PublicPath));
 
@@ -9,6 +10,6 @@ app.use((req, res) => {
     res.sendFile(path.join(PublicPath, "index.html"));
 })
 
-app.listen(3000, () => {
+app.listen(port, () => {
     console.log("server is running on port 3000");
 })

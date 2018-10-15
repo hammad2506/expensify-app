a0894beb5044a4caecef7c6886dc5c25af8d7e48
Hammad Balkhi <hammadbalkhi@Hammads-MacBook-Pro.local>
2018-10-14 20:26:47 -0400

    Added Dev Dependencies

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ module.exports = (env) => {
   return {
     entry: './src/app.js',
     output: {
-      path: path.join(__dirname, 'public'),
+      path: path.join(__dirname, 'public', 'dist'),
       filename: 'bundle.js'
     },
     module: {
@@ -29,7 +29,8 @@ module.exports = (env) => {
     devtool: productionMode ? 'source-map' : 'inline-source-map',
     devServer: {
       contentBase: path.join(__dirname, 'public'),
-      historyApiFallback: true
+      historyApiFallback: true,
+      publicPath: '/dist/'
     }
   } 
 }
